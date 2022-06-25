c7022b001c19acbac49570bf07a8c8b1f1f5b700
oguzhan coruk <coruk_61@hotmail.com>
2022-06-25 17:40:42 +0300

    file change

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-const fn=function() {
-    console.log("Function worling")
+const express = require('express')
+
+const app = express()
+
+const myLogger = (req, res, next) => {
+    console.log("Middleware Log 1")
 }
+app.use(express.static(''))
+app.use(myLogger)
+app.get('/', (req, res) => {
+
+    const photo = {
+        id: 1,
+        name: "photo name",
+        description: "photo"
+    }
+    res.send(photo)
+})
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Sunucu ${port} portunda başlatıldı..`)
+})
